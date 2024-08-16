b9732375b8b0d83c0846efcb763d22c349447a10
Muze <mark46716@gmail.com>
2024-08-15 21:51:48 -0300

    Add function to show books as tables on page
    Iterates through all book objets and display each of its properties and
    its values. Also, checks if the property is defined in Books so functions
    assigned to prototype are not displayed.

--- script.js
+++ script.js
@@ -21,3 +21,32 @@ const roshidere = new Book ('Roshidere', 'IDK lol', '500', 'almost finished');
 addBookToLibrary(cote);
 addBookToLibrary(roshidere);
 
+const body = document.querySelector('body');
+
+function showBooks (library) {
+    for (book of library) {
+        const table = document.createElement('table');
+        
+        console.log('new book');
+        for (prop in book) {
+            if (book.hasOwnProperty(prop)) //Only allows own properties
+            {
+                const row = document.createElement('tr');
+                const data = document.createElement('td');
+                const header = document.createElement('th');
+
+                // Defines hader text as name of properties and data as values
+                header.textContent = `${prop}`;
+                data.textContent = `${book[prop]}`;
+
+                //Appends both th and td to tr and then append tr to table
+                row.appendChild(header);
+                row.appendChild(data);
+                table.appendChild(row);
+            }
+        }
+        body.appendChild(table);
+    }
+}
+
+showBooks(myLibrary);
